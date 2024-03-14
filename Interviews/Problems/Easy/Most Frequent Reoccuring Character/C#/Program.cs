4c89a7b3a1e0c48044b189bad232a6cc34543949
Tyler-Harker <40257850+Tyler-Harker@users.noreply.github.com>
2024-03-14 04:45:36 +0000

    chore: added c# program

---
+++ Interviews/Problems/Easy/Most Frequent Reoccuring Character/C#/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Program.PrintTest("this is a test", 't');
+Program.PrintTest("how about this one", 'o');
+Program.PrintTest("this is another demo", ' ');
+
+static partial class Program {
+    /// <summary>
+    /// Returns the most frequent character in a given string.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns>The most frequent character</returns>
+    public static char? GetMostFrequentCharacterIgnoreCase(string input){
+        Dictionary<char, int> characters = new();
+        foreach(var character in input){
+            if(characters.ContainsKey(character)){
+                characters[character]++;
+            }
+            else{
+                characters.Add(character, 1);
+            }
+        }
+
+        return characters.OrderByDescending(x => x.Value).FirstOrDefault().Key;
+    }
+
+    public static void PrintTest(string input, char expectedCharacter){
+        var result = Program.GetMostFrequentCharacterIgnoreCase(input);
+        if(result == expectedCharacter){
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        else{
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        Console.WriteLine($"Input string: '{input}' most frequent character: '{expectedCharacter}'");
+    }
+}
+
